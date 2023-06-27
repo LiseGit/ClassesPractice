01650fabc5eadcc8fcffc6c2f323aa5a67c1a0c9
Elizaveta Glushchenko <elizaveta.glushchenko@softeq.com>
2023-06-27 23:29:37 +0300

    Commit Add projects

---
+++ ClassesPractice/ShapeAreaCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShapeAreaCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Shape area calculator!");
+        }
+    }
+}
